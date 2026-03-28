aa4556909614288d890756166a5307b28c0afb54
scryner <scryner@gmail.com>
2026-03-28 22:25:01 +0900

    chore(Homebrew formula): update to v0.1.5

--- Formula/my-streamdeck.rb
+++ Formula/my-streamdeck.rb
@@ -1,8 +1,8 @@
 class MyStreamdeck < Formula
   desc "Menu bar controller for an Elgato Stream Deck"
   homepage "https://github.com/scryner/my-streamdeck"
-  url "https://github.com/scryner/my-streamdeck/archive/refs/tags/v0.1.4.tar.gz"
-  sha256 "33e51d7cf33400d25958ea7dab1deeb196baf81507f6f3563bf6369471f6ef03"
+  url "https://github.com/scryner/my-streamdeck/archive/refs/tags/v0.1.5.tar.gz"
+  sha256 "a388003e935e49804c881d59b8877dcffceff012a22413077c23e9a5a401fe7a"
   license "MIT"
   head "https://github.com/scryner/my-streamdeck.git", branch: "main"
 
